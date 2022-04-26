6ffdfc6b47084fa25a91a7dcc8d3b955180e520c
Nishith <nishith.patel@appgambit.com>
2022-04-26 15:55:33 +0530

    Content overlapping issue resolved

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import {Text, SafeAreaView, Pressable} from 'react-native';
+import {Text, SafeAreaView, Pressable, View} from 'react-native';
 import styles from './style';
 import commonStyles from '../../theme/commonStyles';
-import Animated, {FadeIn} from 'react-native-reanimated';
 
 const Home = props => {
   const navigation = props.navigation;
 
   return (
     <SafeAreaView style={commonStyles.flex}>
-      <Animated.View
-        style={[commonStyles.containerPadding, styles.container]}
-        entering={FadeIn.delay(200)}>
+      <View style={[commonStyles.containerPadding, styles.container]}>
         <Text style={styles.screenText}>Home</Text>
         <Pressable
           onPress={() => navigation.navigate('About')}
           style={styles.aboutBtn}>
           <Text style={styles.aboutBtnText}>About</Text>
         </Pressable>
-      </Animated.View>
+      </View>
     </SafeAreaView>
   );
 };
